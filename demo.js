d2824c5ce32a25f015b91b088ff807d61fb0cb91
sebastian-leandro <sebas.officciall@gmail.com>
2023-08-29 12:00:05 -0300

    Working with the chat demo

--- demo.js
+++ demo.js
@@ -1,20 +1,25 @@
-const homeButton = document.getElementById("home--button");
-const worksButton = document.getElementById("works--button");
-const worksSection = document.getElementById("works--section");
-const closeSections = document.getElementById("close--sections");
-
-worksButton.addEventListener("click", () => {
-    worksSection.classList.add("show");
-});
-
-closeSections.addEventListener("click", () => {
-    worksSection.classList.remove("show")
-})
-
-homeButton.addEventListener("click", () => {
-    worksSection.classList.remove("show")
-})
-
-
-
-
+const sectionMap = {
+    "works--button": "works--section",
+    "talents--button": "talents--section",
+    "transactions--button": "transactions--section"
+  };
+  
+  const sectionsArray = ["works--section", "talents--section", "transactions--section"];
+  const closeButtons = ["close--sections", "close--talents", "close--transations"];
+  
+  function hideAll() {
+    sectionsArray.forEach(id => document.getElementById(id).classList.remove("show"));
+  }
+  
+  document.getElementById("home--button").addEventListener("click", hideAll);
+  
+  Object.keys(sectionMap).forEach(key => {
+    document.getElementById(key).addEventListener("click", () => {
+      hideAll();
+      document.getElementById(sectionMap[key]).classList.add("show");
+    });
+  });
+  
+  closeButtons.forEach(id => {
+    document.getElementById(id).addEventListener("click", hideAll);
+  });
