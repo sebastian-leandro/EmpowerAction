6b9eeb2b28d16dacecf52ffb2f5e8e8925b3055f
sebastian-leandro <sebas.officciall@gmail.com>
2023-08-30 14:50:55 -0300

    Working with the talent section

--- demo.js
+++ demo.js
@@ -23,3 +23,88 @@ const sectionMap = {
   closeButtons.forEach(id => {
     document.getElementById(id).addEventListener("click", hideAll);
   });
+
+  $(document).ready(function(){
+    $('#action_menu_btn').click(function(){
+      $('.action_menu').toggle();
+    });
+  });
+
+
+
+
+
+  //append 
+
+const windowPost = document.getElementById("window--post");
+const windowBtn = document.getElementById("talent--post");
+const windowCloseBtn = document.getElementById("close--post");
+const postTalent = document.getElementById("post--talent");
+
+
+
+windowBtn.addEventListener("click", () => {
+  windowPost.classList.add("show");
+});
+
+windowCloseBtn.addEventListener("click", () => {
+  windowPost.classList.remove("show");
+});
+
+postTalent.addEventListener("click", function CreateTalent() {
+  function getValuesTalent(){
+      let inputName = document.getElementById("postTalentInput-1").value;
+      let inputPrice = document.getElementById("postTalentInput-2").value;
+      return {
+          name: inputName,
+          price: inputPrice
+      };  
+  }
+
+  const talentValues = getValuesTalent();
+
+  if (talentValues.name && talentValues.price) {
+      const createTalentPost = document.querySelector(".grid--talents--section");
+
+      // Main div
+      let itemTalentDiv = document.createElement("div"); 
+      itemTalentDiv.classList.add("grid--works--item");
+
+      // Profile div
+      let itemTalentDivProfile = document.createElement("div");
+      itemTalentDivProfile.classList.add("profile--worker");
+
+      // Image
+      let itemTalentImg = document.createElement("img");
+      itemTalentImg.src= "./img/face.png";
+      itemTalentImg.classList.add("img-styling");
+
+      // Name
+      let itemTalentH3 = document.createElement("h3");
+      itemTalentH3.classList.add("h3-styling");
+      itemTalentH3.innerText = talentValues.name;
+
+      // Price
+      let itemTalentP = document.createElement("p");
+      itemTalentP.classList.add("p-styling");
+      itemTalentP.innerText = talentValues.price;
+
+      // Appending elements
+      itemTalentDivProfile.appendChild(itemTalentImg);
+      itemTalentDivProfile.appendChild(itemTalentH3);
+      itemTalentDiv.appendChild(itemTalentDivProfile);
+      itemTalentDiv.appendChild(itemTalentP);
+      createTalentPost.appendChild(itemTalentDiv);
+
+
+      windowPost.classList.remove("show");
+
+  } else {
+      window.alert("Los campos no pueden estar vacios");
+  }
+});
+
+
+
+
+
